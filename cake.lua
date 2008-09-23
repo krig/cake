dd1e44e66bf6d76ebea4493b69578cae3f38ab54
Kristoffer Grönlund <kristoffer.gronlund@purplescout.se>
2008-09-23 12:58:01 +0200

    simplified cake.lua for cake itself (no longer depends on functions defined in auto.lua)

--- cake.lua
+++ cake.lua
@@ -1,42 +1,5 @@
---setup_scoria()
-
-p = application("cake")
-use_lua(p)
-
-
---[[
-p:add_flags("-Icake/lua")
-p:add_files(
-	    "cake/lua/lapi.c",
-	    "cake/lua/lcode.c",
-	    "cake/lua/ldebug.c",
-	    "cake/lua/ldo.c",
-	    "cake/lua/ldump.c",
-	    "cake/lua/lfunc.c",
-	    "cake/lua/lgc.c",
-	    "cake/lua/llex.c",
-	    "cake/lua/lmem.c", 
-	    "cake/lua/lobject.c",
-	    "cake/lua/lopcodes.c",
-	    "cake/lua/lparser.c",
-	    "cake/lua/lstate.c",
-	    "cake/lua/lstring.c",
-	    "cake/lua/ltable.c",
-	    "cake/lua/ltm.c", 
-	    "cake/lua/lundump.c",
-	    "cake/lua/lvm.c",
-	    "cake/lua/lzio.c",
-	    "cake/lua/lauxlib.c",
-	    "cake/lua/lbaselib.c",
-	    "cake/lua/ldblib.c",
-	    "cake/lua/liolib.c",
-	    "cake/lua/lmathlib.c",
-	    "cake/lua/loslib.c",
-	    "cake/lua/ltablib.c", 
-	    "cake/lua/lstrlib.c",
-	    "cake/lua/loadlib.c",
-	    "cake/lua/linit.c")
---]]
+p = program("cake")
+p:add_pkgdepend("lua5.1")
 
 p:add_files("cake/stdafx.h",
     	    "cake/*.cpp")
